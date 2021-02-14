2e4352b2d6295a36de14ab70b42c246a8d169814
ylldesmustafa <77593205+ylldesmustafa@users.noreply.github.com>
2021-02-14 16:58:33 -0600

    added component files

---
+++ my-store/src/data.js
@@ -0,0 +1,110 @@
+/* 
+
+*/
+
+
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Cat Toy 1", 
+    img: "img/catToy1.png",
+    price: 5,
+    company: "Heads-To-Tails Pet Shop",
+    info: 
+    "lorem ipsum dolor amet offal butcher quiona sustainable gastropub, echp park acctual green juice sriracha paleo"
+  },
+  {
+    id: 2,
+    title: "Cat Toy 2", 
+    img: "img/catToy2.png",
+    price: 5,
+    company: "Heads-To-Tails Pet Shop",
+    info: 
+    "lorem ipsum dolor amet offal butcher quiona sustainable gastropub, echp park acctual green juice sriracha paleo"
+  },
+  {
+    id: 3,
+    title: "Cat Toy 3", 
+    img: "img/catToy3.png",
+    price: 5,
+    company: "Heads-To-Tails Pet Shop",
+    info: 
+    "lorem ipsum dolor amet offal butcher quiona sustainable gastropub, echp park acctual green juice sriracha paleo"
+  },
+  {
+    id: 4,
+    title: "Cat Toy 4", 
+    img: "img/catToy4.png",
+    price: 5,
+    company: "Heads-To-Tails Pet Shop",
+    info: 
+    "lorem ipsum dolor amet offal butcher quiona sustainable gastropub, echp park acctual green juice sriracha paleo"
+  },
+  {
+    id: 5,
+    title: "Cat Toy 5", 
+    img: "img/catToy5.png",
+    price: 5,
+    company: "Heads-To-Tails Pet Shop",
+    info: 
+    "lorem ipsum dolor amet offal butcher quiona sustainable gastropub, echp park acctual green juice sriracha paleo"
+  },
+  {
+    id: 6,
+    title: "Cat Toy 6", 
+    img: "img/catToy6.png",
+    price: 5,
+    company: "Heads-To-Tails Pet Shop",
+    info: 
+    "lorem ipsum dolor amet offal butcher quiona sustainable gastropub, echp park acctual green juice sriracha paleo"
+  },
+  {
+    id: 7,
+    title: "Cat Toy 7", 
+    img: "img/catToy7.png",
+    price: 5,
+    company: "Heads-To-Tails Pet Shop",
+    info: 
+    "lorem ipsum dolor amet offal butcher quiona sustainable gastropub, echp park acctual green juice sriracha paleo"
+  },
+  {
+    id: 8,
+    title: "Cat Toy 8", 
+    img: "img/catToy8.png",
+    price: 5,
+    company: "Heads-To-Tails Pet Shop",
+    info: 
+    "lorem ipsum dolor amet offal butcher quiona sustainable gastropub, echp park acctual green juice sriracha paleo"
+  },
+  {
+    id: 9,
+    title: "Cat Toy 9", 
+    img: "img/catYot9.png",
+    price: 5,
+    company: "Heads-To-Tails Pet Shop",
+    info: 
+    "lorem ipsum dolor amet offal butcher quiona sustainable gastropub, echp park acctual green juice sriracha paleo"
+  },
+  {
+    id: 10,
+    title: "Cat Toy 10", 
+    img: "img/catToy10.png",
+    price: 5,
+    company: "Heads-To-Tails Pet Shop",
+    info: 
+    "lorem ipsum dolor amet offal butcher quiona sustainable gastropub, echp park acctual green juice sriracha paleo"
+  }
+];
+
+export const detailProduct = {
+  id: 1, 
+  title: "Cat Toy 1",
+  img: "img/catToy1.png",
+  price: 5, 
+  company: "Heads-To-Tails Pet Shop",
+  info:
+    "lorem ipsum dolor amet offal butcher quiona sustainable gastropub, echp park acctual green juice sriracha paleo",
+  inCart: false, 
+  count: 0,
+  total: 0
+};
