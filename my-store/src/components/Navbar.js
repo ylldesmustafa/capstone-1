ea13df20a1e1ca011d6d301e3d351b2bcbd2f62b
ylldesmustafa <77593205+ylldesmustafa@users.noreply.github.com>
2021-02-14 20:09:49 -0600

    created title.js

--- my-store/src/components/Navbar.js
+++ my-store/src/components/Navbar.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
-import logo from '../catLogo.svg'
-
-
+import logo from '../catLogo.svg';
+import styled from 'styled-components';
+import {ButtonContainer} from "./Button";
 
 export default class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+      <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
        { /* 
         https://www.iconfinder.com/icons/751111/cats_cat_dogs_pet_pets_icon Creative Commons (Attribution 3.0 Unported); https://www.iconfinder.com/pinpointworld
         */}
@@ -22,12 +22,23 @@ export default class Navbar extends Component {
           </li>
         </ul>
         <Link to ="./cart"className="ml-auto">
-          <button>
+          <ButtonContainer>
+            <span className="mr-2">
             <i className="fas fa-cart-plus" />
+            </span>
             My Cart
-          </button>
+          </ButtonContainer>
         </Link>
-      </nav>
+      </NavWrapper>
     )
   }
 }
+
+const NavWrapper = styled.nav`
+  background:var(--mainBlue);
+  .nav-link{
+    color:var(--mainWhite) !important;
+    font-size:1.3rem;
+    text-transform:capitalize;
+  }
+`;
