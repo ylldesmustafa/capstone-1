1f9c0d84dbdd26a8032055a8c86d3cc78b7d1b65
ylldesmustafa <77593205+ylldesmustafa@users.noreply.github.com>
2021-02-14 21:36:23 -0600

    added product variables

--- my-store/src/components/Product.js
+++ my-store/src/components/Product.js
@@ -1,11 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react'; 
+import styled from 'styled-components';
+import {Link} from 'react-router-dom';
+import {ProductConsumer} from '../context';
 
 export default class Product extends Component {
   render() {
+    const {id, title, img, price, inCart} = this.props.Product;
     return (
-      <div>
-        <h3>hello from product</h3>
-      </div>
-    )
+      <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3">
+        <div className="card">
+        <div className="img-container p-5" onClick={console.log('you clicked me on the image container')
+      }>
+      <Link to="/details">
+        <img src={img} alt="product" className="card-img-top"/>
+      </Link>
+        </div>
+        </div>
+      </ProductWrapper>
   }
 }
+
+const ProductWrapper = styled.div`
+`
