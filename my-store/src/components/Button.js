91775c8faeb1a99142ed533e90edaa1d908040d5
ylldesmustafa <77593205+ylldesmustafa@users.noreply.github.com>
2021-02-15 23:07:07 -0600

    adding css to add to cart buttons

--- my-store/src/components/Button.js
+++ my-store/src/components/Button.js
@@ -5,7 +5,9 @@ export const ButtonContainer = styled.button`
   font-size:1.4rem;
   background:transparent;
   border:0.05rem solid var(--lightBlue);
-  color:var(--lightBlue);
+  border-color: ${props => 
+    props.cart ? "var--(mainYellow)" : "var(--lightBlue)"};
+  color: ${prop => prop.cart ? "var)--mainYellow" : "var(--lightBlue)"};
   border-radius:0.5rem;
   padding: 0.2rem 0.5rem;
   cursor:pointer;
@@ -15,7 +17,7 @@ export const ButtonContainer = styled.button`
     background:var(--lightBlue);
     color:var(--mainBlue);
   }
-  &::focus{
+  &:focus{
     outline:none;
   }
 `;
--- my-store/src/components/Details.js
+++ my-store/src/components/Details.js
@@ -2,19 +2,20 @@ import React, { Component } from 'react';
 import { ProductConsumer } from '../context';
 import { Link } from "react-router-dom";
 import { ButtonContainer } from './Button';
+import { Button } from 'bootstrap';
 
 export default class Details extends Component {
   render() {
     return (
       <ProductConsumer>
         {(value) =>{
-          const {id,company,img,info,price,title,inCart} = value.detailProduct;
+          const {id,company,img,info,title,price,inCart} = value.detailProduct;
           return (
             <div className="container py-5">
               {/* title */}
               <div className="row">
               <div className="col-10 mx-auto text-center text-slanted text-blue my-5">
-                <h1>{title}</h1>
+                <h1>Heads-To-Tails Pet Shop</h1>
               </div>
               </div>
               {/* end title */}
@@ -25,7 +26,32 @@ export default class Details extends Component {
                 </div>
                 {/* product text */}
                 <div className="col-10 mx auto col-md-6 my-3 text-capitalize">
-
+                <h2>{title}</h2>
+                <p className="text-muted lead">{info}</p>
+                <h4 className="text-blue">
+                  <strong>
+                  Price: <span>$</span>
+                    {price}
+                  </strong>
+                </h4>
+                <h4 className="text-title text-uppercase text-muted mt-3 mb-2">
+                  Made by: <span className="text-uppercase">{company}</span>
+                </h4>
+                {/* Buttons */}
+                <div>
+                  <Link to='/'>
+                    <ButtonContainer>Back to Product Page</ButtonContainer>
+                  </Link>
+                  <ButtonContainer 
+                  cart 
+                  disabled={inCart ? true : false}
+                  onClick={() =>{
+                    value.addToCart(id);
+                  }}
+                  >
+                    {inCart ? "inCart" : "add to cart"}
+                  </ButtonContainer>
+                </div>
                 </div>
               </div>
             </div>
