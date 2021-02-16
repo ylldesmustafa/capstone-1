37eae13f684219893107976d5ab2dd23ca4988ee
ylldesmustafa <77593205+ylldesmustafa@users.noreply.github.com>
2021-02-15 20:03:44 -0600

    added product images to details.js

--- my-store/src/components/Details.js
+++ my-store/src/components/Details.js
@@ -18,6 +18,16 @@ export default class Details extends Component {
               </div>
               </div>
               {/* end title */}
+              {/* product information */}
+              <div className="row">
+                <div className="col-10 mx auto col-md-6 my-3">
+                  <img src={img} className="img-fluid" alt="product" />
+                </div>
+                {/* product text */}
+                <div className="col-10 mx auto col-md-6 my-3 text-capitalize">
+
+                </div>
+              </div>
             </div>
           )
         }}
