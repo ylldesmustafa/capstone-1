89d96de9394b24de0630aabb213f25c03fa0c974
ylldesmustafa <77593205+ylldesmustafa@users.noreply.github.com>
2021-02-22 08:12:33 -0600

    added clear cart button to CartTotals.js to create clear cart function

--- my-store/src/components/Cart/CartTotals.js
+++ my-store/src/components/Cart/CartTotals.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-export default function CartTotals() {
+export default function CartTotals({value}) {
+  const {cartSubtotal, cartTax, cartTotal, clearCart} = value;
   return (
-    <div>
-      
-    </div>
-  )
+   <React.Fragment>
+     <div className="container">
+      <div className="row">
+      <div className="col-10 mt-2 ml-sm-5 ml-md-auto col-sm-8 text-capitalize text-right">
+        <Link to="/">
+          <button className="btn btn-outline-danger text-uppercase mb-3 px-5" type="button" onClick={() => clearCart()}>
+            Clear Cart
+          </button>
+        </Link>
+      </div>
+      </div>
+     </div>
+   </React.Fragment>
+  );
 }
