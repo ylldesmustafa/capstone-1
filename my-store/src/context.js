43b074d8774c1e6728c83e1816be74c765a5231c
ylldesmustafa <77593205+ylldesmustafa@users.noreply.github.com>
2021-02-23 00:36:27 -0600

    added decrement function to item quantity in cart

--- my-store/src/context.js
+++ my-store/src/context.js
@@ -83,8 +83,27 @@ increment = (id) => {
   () => {this.addTotals()});
 }
 decrement = (id) => {
-  console.log('this is the decrement method');
+  let tempCart = [...this.state.cart];
+  const selectedProduct = tempCart.find(item => item.id === id);
 
+  const index = tempCart.indexOf(selectedProduct);
+  const product = tempCart[index];
+  product.count = product.count - 1;
+
+  if (product.count === 0) {
+    this.removeItem(id)
+  }
+  else {
+    product.total = product.count * product.price;
+    this.setState(
+      () => {
+        return { cart: [...tempCart] };
+      }, 
+      () => {
+        this.addTotals();
+      }
+      );
+  }
 }
 removeItem = (id) => {
   let tempProducts = [...this.state.products];
