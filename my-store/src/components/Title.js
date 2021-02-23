8ebda577bbc74cdd18c4ccea2cc750302dba3813
ylldesmustafa <77593205+ylldesmustafa@users.noreply.github.com>
2021-02-23 06:51:59 -0600

    adding new CSS to project

--- my-store/src/components/Title.js
+++ my-store/src/components/Title.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-export default function Title({name,title}) {
+export default function Title({title}) {
   return (
     <div className="row">
       <div className="col-10 mx-auto my-2 text-center text-title">
         <h1 className="text-capitalize font-weight-bold">
-          {name} <strong className="text-blue"> {title}
+          HEADS-TO-TAILS Pet Shop <strong className="text-blue"> {title}
 
           </strong>
         </h1>
