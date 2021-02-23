2312b54a0ed105ab70143d033e65bf7a508fad95
ylldesmustafa <77593205+ylldesmustafa@users.noreply.github.com>
2021-02-23 00:48:08 -0600

    added error page not found component to default.js

--- my-store/src/components/Default.js
+++ my-store/src/components/Default.js
@@ -3,8 +3,14 @@ import React, { Component } from 'react';
 class Default extends Component {
   render() {
     return (
-      <div>
-        <h3>page not found</h3>
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto text-center text-title text-uppercase pt-5">
+              <h1 className="display-3">ERROR</h1>
+              <h1 className="display-3">PAGE NOT FOUND</h1>
+              <h3>The requested URL <span className="text-danger">{this.props.location.pathname}</span> is  not found.</h3>
+          </div>
+        </div>
       </div>
     )
   }
