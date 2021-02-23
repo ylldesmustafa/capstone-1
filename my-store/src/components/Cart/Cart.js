504d9f0d26ed5a8b591eec6fc513fb5a68ecc8c6
ylldesmustafa <77593205+ylldesmustafa@users.noreply.github.com>
2021-02-23 07:23:38 -0600

    implemented PayPal button and functionality

--- my-store/src/components/Cart/Cart.js
+++ my-store/src/components/Cart/Cart.js
@@ -19,7 +19,7 @@ class Cart extends Component {
                   <Title name="your" title="cart"></Title>
                   <CartColumns></CartColumns>
                   <CartList value={value}></CartList>
-                  <CartTotals value={value}></CartTotals>
+                  <CartTotals value={value} history={this.props.history}></CartTotals>
                   </section>
               );
             }
