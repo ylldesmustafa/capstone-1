f39cb3ee4deb412cea67c98e9bd1434fc45a306c
ylldesmustafa <77593205+ylldesmustafa@users.noreply.github.com>
2021-02-23 00:25:07 -0600

    added increment function (increase product quantity in cart)  to context.js

--- my-store/src/context.js
+++ my-store/src/context.js
@@ -70,20 +70,49 @@ this.setState(() => {
 });
 };
 increment = (id) => {
-  console.log('this is the increment method');
+  let tempCart = [...this.state.cart];
+  const selectedProduct = tempCart.find(item => item.id === id);
+
+  const index = tempCart.indexOf(selectedProduct);
+  const product = tempCart[index];
+
+  product.count = product.count + 1;
+  product.total = product.count * product.price;
+
+  this.setState(() => {return {cart: [...tempCart]}}, 
+  () => {this.addTotals()});
 }
 decrement = (id) => {
   console.log('this is the decrement method');
 
 }
 removeItem = (id) => {
-  console.log('item removed');
+  let tempProducts = [...this.state.products];
+  let tempCart = [...this.state.cart];
+
+  tempCart = tempCart.filter(item => item.id !== id);
+
+  const index = tempProducts.indexOf(this.getItem(id));
+  let removedProduct = tempProducts[index];
+  removedProduct.inCart = false;
+  removedProduct.inCart = 0;
+  removedProduct.total = 0;
+
+  this.setState(() => {
+    return {
+      cart:[...tempCart],
+      products: [...tempProducts],
+    }
+  }, () => {
+    this.addTotals();
+  })
 }
 clearCart = () => {
   this.setState(() => {
     return { cart: [] };
     }, () => {
       this.setProducts();
+      this.addTotals();
     });
 };
 addTotals = () => {
